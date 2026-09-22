d64cfa75b48a4fe94c0c2cce49a7ab428bdc5a1b
esmael hamdy <esmealhamdy404@gmail.com>
2026-09-23 00:47:08 +0200

    Add project files.

---
+++ c#Basics05/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_Basics05
+{
+    internal class Book
+    {
+      private string password = "secret";
+       internal int CopiesInstock = 10;
+    }
+}
---
+++ c#Basics05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace c_Basics05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question 1
+
+            //Book p = new Book();
+            //'Book.password' is inaccessible due to its protection level ظهر هذا الخطأ بسبب ان المتغير password معرف ك private داخل الكلاس Book و لا يمكن الوصول اليه من خارج الكلاس.
+
+            //Console.WriteLine($"the password is: = {p.password}");
+            #endregion
+            #region question 2
+            //Book book = new Book();
+            //Console.WriteLine($"copies in stock: = {book.CopiesInstock}");
+            ////تمت الطباعه لان scope المتغير CopiesInstock معرف ك internal داخل الكلاس Book و يمكن الوصول اليه من نفس المشروع.
+
+            #endregion
+        }
+    }
+}
